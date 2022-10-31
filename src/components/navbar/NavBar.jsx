772722e465f639dbefead4a7e54c48fb2b1872f7
Degisew <jegisew21@gmail.com>
2022-10-31 11:29:40 +0300

    added a shoping cart

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  Typography,
+} from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import styles from "./styles";
+import logo from "../../assets/images/logo.png";
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <AppBar position="fixed" color="inherit" sx={styles.appBar}>
+        <Toolbar sx={styles.toolBar}>
+          <Typography variant="h6" sx={styles.logoTitle}>
+            <img src={logo} alt="Logo" width="35px" sx={styles.image} />
+            Ethio-Market
+          </Typography>
+          <div>
+            <IconButton aria-label="show-cart-items">
+              <Badge badgeContent={3} color="error">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
