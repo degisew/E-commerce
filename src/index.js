9b33f587a6bb654edb29e084a578bcf584604638
Degisew <jegisew21@gmail.com>
2022-10-31 11:22:46 +0300

    Added a default theme customization for MUI components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material";
+import "./index.css";
+
+const theme = createTheme({
+  typography: {
+    body2: {
+      fontWeight: "normal",
+      fontSize: '13px'
+    },
+  },
+});
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
