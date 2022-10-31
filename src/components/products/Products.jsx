7547281bc6007eea98bd0edcffe2f6422ec51fe0
Degisew <jegisew21@gmail.com>
2022-10-31 11:30:33 +0300

    passed a data from api to product component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,39 +1,41 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import Product from "./product/Product";
-const Products = () => {
-  const products = [
-    {
-      id: 1,
-      name: "shoes",
-      price: "$50",
-      image:
-        "https://sp.yimg.com/ib/th?id=OP.d%2bhKdRtwJcIAOg474C474&o=5&pid=21.1",
-      description: "This is a shoes",
-    },
-    {
-      id: 2,
-      name: "T-shirt",
-      price: "$80",
-      image:
-        "https://sp.yimg.com/ib/th?id=OP.WLR17%2fJNuCk1qQ474C474&o=5&pid=21.1",
-      description: "This is a T-shirt",
-    },
-    {
-      id: 3,
-      name: "Jacket",
-      price: "$150",
-      image:
-        "https://sp.yimg.com/ib/th?id=OP.zEt4D%2bPQPSSe4w474C474&o=5&pid=21.1",
-      description: "This is a jack",
-    },
-  ];
+import Product from "./Product/Product";
+import styles from "./styles";
+const Products = ({ products}) => {
+  // const products = [
+  //   {
+  //     id: 1,
+  //     name: "shoes",
+  //     price: "$50",
+  //     image:
+  //       "https://sp.yimg.com/ib/th?id=OP.d%2bhKdRtwJcIAOg474C474&o=5&pid=21.1",
+  //     description: "This is a shoes",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "T-shirt",
+  //     price: "$80",
+  //     image:
+  //       "https://sp.yimg.com/ib/th?id=OP.WLR17%2fJNuCk1qQ474C474&o=5&pid=21.1",
+  //     description: "This is a T-shirt",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Jacket",
+  //     price: "$150",
+  //     image:
+  //       "https://sp.yimg.com/ib/th?id=OP.zEt4D%2bPQPSSe4w474C474&o=5&pid=21.1",
+  //     description: "This is a jack",
+  //   },
+  // ];
   return (
     <main>
       <Grid
+        sx={styles.root}
         container
         justifyContent="center"
-        rowSpacing={1}
+        rowSpacing={3}
         display="flex"
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
       >
