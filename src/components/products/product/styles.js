8f92a4a2b87bfa7277afff6ba550e268ad9f2ced
Degisew <jegisew21@gmail.com>
2022-10-31 11:33:09 +0300

    Added a style for product items

--- src/components/products/product/styles.js
+++ src/components/products/product/styles.js
@@ -1,17 +1,20 @@
 const styles = {
   root: {
     maxWidth: "100%",
+    minHeight: "450px",
   },
 
   media: {
     paddingTop: "56.25%",
     height: 0,
+    margin: "10px auto",
+    width: "60%",
   },
 
   cardContent: {
     display: "flex",
     padding: 0,
-    justifyContent: "space-between",
+    justifyContent: "space-between",  
   },
 
   cardActions: {
