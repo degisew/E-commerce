1aa422384f2742dd0092a6c36ab2ff00cb613a94
Degisew <jegisew21@gmail.com>
2022-11-04 13:16:18 +0300

    added a router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter as Router } from 'react-router-dom'
 import "./index.css";
 
 const theme = createTheme({
   typography: {
     body2: {
       fontWeight: "normal",
-      fontSize: '13px'
+      fontSize: "13px",
     },
   },
 });
 const root = createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </ThemeProvider>
 );
