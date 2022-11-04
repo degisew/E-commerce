9bf28f0686adb06467f468d259d6fa73522764c0
Degisew <jegisew21@gmail.com>
2022-11-04 16:57:07 +0300

    added a style for card media

---
+++ src/components/Cart/CartItem/styles.js
@@ -0,0 +1,26 @@
+
+
+const styles = {
+  card: {
+    marginBottom: "1rem"
+  },
+  media: {
+    paddingTop: "56.25%",
+    height: 0,
+    margin: "10px auto",
+    width: "60%",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+    paddingLeft: "5px",
+    paddingRight: "5px",
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+};
+
+
+export default styles;
