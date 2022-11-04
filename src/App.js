9cdb53e66085c806cf4860b6ef009390f2494096
Degisew <jegisew21@gmail.com>
2022-11-04 13:18:24 +0300

    added a router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./components/Navbar/NavBar";
 import Products from "./components/Products/Products";
 import commerce from "./lib/commerce";
+import Cart from "./components/Cart/Cart";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   // states
   const [products, setProducts] = useState([]);
@@ -23,19 +25,30 @@ const App = () => {
   };
   // Add item to cart
   const addToCartHandler = async (productId, quantity) => {
-    const item = commerce.cart.add(productId, quantity);
+    const item = await commerce.cart.add(productId, quantity);
     setCart(item);
   };
   // state lifecycle method with hooks
   useEffect(() => {
-    fetchCart();
+    // deleteCart();
     fetchProducts();
+    fetchCart();
   }, []);
-  console.log(cart);
+
+  //console.log(cart);
+
   return (
     <>
-      <NavBar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={addToCartHandler} />
+        <NavBar totalItems={cart.total_items} />
+      <Routes>
+        <Route path="/" component={<Cart cart={cart} products={products} />} />
+        <Route
+          path="/cart"
+          component={
+            <Products products={products} onAddToCart={addToCartHandler} />
+          }
+        />
+      </Routes>
     </>
   );
 };
