b21474420a621634007e910f8ba39823c0990629
Degisew <jegisew21@gmail.com>
2022-11-04 22:01:57 +0300

    Added a functionality for add, remove, and empty cart buttons

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,18 +5,21 @@ import {
   Grid,
   Typography,
   Button,
+  Tooltip,
 } from "@mui/material";
+import { RemoveShoppingCart, PriceCheck } from "@mui/icons-material";
 import CartItem from "./CartItem/CartItem";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, updateQty, removeProduct, emptyCart }) => {
   // console.log(cart.line_items.length)
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
-      You have no items yet, 
-      <Link to='/'> 
-      start adding them!
-      </Link>
+      You have no items yet,
+      <Tooltip title="click to add items">
+
+      <Link to="/" className='add-items'>start adding them!</Link>
+      </Tooltip>
     </Typography>
   );
   const FilledCart = () => (
@@ -24,22 +27,36 @@ const Cart = ({ cart }) => {
       <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={item.id}>
-            <CartItem item={item} />
+            <CartItem
+              item={item}
+              updateQty={updateQty}
+              removeProduct={removeProduct}
+            />
           </Grid>
         ))}
       </Grid>
+      <div className="subtotal-btns-div">
       <div className="subTotal">
-        <Typography variant="h4">
+        <Typography variant="h5" color="black">
           Subtotal: {cart.subtotal.formatted_with_symbol}
         </Typography>
       </div>
       <div className="btn-cart">
-        <Button color="secondary" variant="contained" size="large">
+        <Button
+          startIcon={<RemoveShoppingCart />}
+          color="error"
+          variant="contained"
+          size="small"
+          onClick={() => {
+            emptyCart();
+          }}
+        >
           EMPTY CART
         </Button>
-        <Button color="primary" variant="contained" size="large">
+        <Button color="primary" variant="contained" size="small" startIcon={<PriceCheck/>}>
           CHECKOUT
         </Button>
+      </div>
       </div>
     </>
   );
@@ -50,7 +67,9 @@ const Cart = ({ cart }) => {
     <>
       <Container>
         <div className="spacerDiv"></div>
-        <Typography variant="h4" gutterBottom >Your Shopping Cart</Typography>
+        <Typography variant="h4" gutterBottom>
+          Your Shopping Cart
+        </Typography>
         {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
       </Container>
     </>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -8,33 +8,51 @@ import {
   Button,
 } from "@mui/material";
 
-import styles from './styles'
-const CartItem = ({ item }) => {
+import styles from "./styles";
+import { Delete } from "@mui/icons-material";
+const CartItem = ({ item, updateQty, removeProduct }) => {
   return (
     <>
       <Card sx={styles.card}>
-        <CardMedia
-          image={item.image.url}
-          title={item.name}
-          sx={styles.media}
-        />
+        <CardMedia image={item.image.url} title={item.name} sx={styles.media} />
         <CardContent sx={styles.cardContent}>
-          <Typography variant="h5">"{item.name}"</Typography>
+          <Typography variant="h5">{item.name.toString()}</Typography>
           <Typography variant="h6">
-            "{item.line_total.formatted_with_symbol}"
+            {item.line_total.formatted_with_symbol}
           </Typography>
         </CardContent>
         <CardActions sx={styles.cardActions}>
           <div className="btn-quantity">
-            <Button type="button" size="large">
+            <Button
+              type="button"
+              size="large"
+              onClick={() => {
+                updateQty(item.id, item.quantity - 1);
+              }}
+            >
               -
             </Button>
-            <Typography variant='body2'>{item.quantity}</Typography>
-            <Button type="buton" size="large">
+            <Typography variant="body2">{item.quantity}</Typography>
+            <Button
+              type="buton"
+              size="large"
+              onClick={() => {
+                updateQty(item.id, item.quantity + 1);
+              }}
+            >
               +
             </Button>
           </div>
-          <Button variant="contained" type="button" color="error">
+          <Button
+          startIcon={<Delete/>}
+            variant="contained"
+            type="button"
+            size="small"
+            color="error"
+            onClick={() => {
+              removeProduct(item.id);
+            }}
+          >
             Remove
           </Button>
         </CardActions>
