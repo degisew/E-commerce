9b9ed5351ffb4ef89fb0c889f560e37e2089e12c
Degisew <jegisew21@gmail.com>
2022-11-04 21:59:01 +0300

    added a style for loading spinner

--- src/components/products/styles.js
+++ src/components/products/styles.js
@@ -2,8 +2,13 @@
 const styles = {
   root: {
     flexGrow: 1,
-    marginTop: '5rem',
+    marginTop: "5rem",
   },
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+     marginTop: "30%"
+  }
 };
 
 export default styles;
