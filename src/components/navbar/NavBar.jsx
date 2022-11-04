16a5123ba3ffba9909eb4645e979c87b4ba48d0d
Degisew <jegisew21@gmail.com>
2022-11-04 16:56:23 +0300

    added a cart item display logic based on location

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,33 +1,37 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Badge,
-  Typography,
-} from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import { AppBar, Toolbar, IconButton, Badge, Typography } from "@mui/material";
 import { ShoppingCart } from "@mui/icons-material";
 import styles from "./styles";
 import logo from "../../assets/images/logo.png";
 const NavBar = ({ totalItems }) => {
+  const location = useLocation();
   return (
     <>
       <AppBar position="fixed" sx={styles.appBar}>
         <Toolbar sx={styles.toolBar}>
-          <Typography variant="h6" sx={styles.logoTitle}>
+          <Typography
+            variant="h6"
+            sx={styles.logoTitle}
+            component={Link}
+            to="/"
+          >
             <img src={logo} alt="Logo" width="35px" sx={styles.image} />
             Ethio-Market
           </Typography>
-          <div>
-            <IconButton aria-label="show-cart-items">
-              <Badge badgeContent={totalItems} color="error">
-                <Link to='/cart'>
+          {location.pathname === "/" && (
+            <div>
+              <IconButton
+                aria-label="show-cart-items"
+                component={Link}
+                to="/cart"
+              >
+                <Badge badgeContent={totalItems} color="error">
                   <ShoppingCart />
-                </Link>
-              </Badge>
-            </IconButton>
-          </div>
+                </Badge>
+              </IconButton>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </>
