cea00f236ce80e7e9a1a3b600dc74243f8da24f7
Degisew <jegisew21@gmail.com>
2022-11-18 15:11:35 +0300

    fixed linter issue

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,45 +1,37 @@
 import React, { useState } from "react";
-import {
-  Paper,
-  Stepper,
-  Step,
-  StepLabel,
-  Typography,
-} from "@mui/material";
+import { Paper, Stepper, Step, StepLabel, Typography } from "@mui/material";
 import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 
-import styles from './styles'
+import styles from "./styles";
 
-const steps = ['shipping address', 'payment details'];
+const steps = ["shipping address", "payment details"];
 
 const Checkout = () => {
-    const [activeStep,setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(0);
+  console.log(setActiveStep(1));
+  const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
 
-    const Form = () => (
-      activeStep === 0 ? <AddressForm/> : <PaymentForm/>
-    );
-
-    const Confirmation = () =>  (
-      <div>Confirmation</div>
-    )
+  const Confirmation = () => <div>Confirmation</div>;
   return (
-  <>
-  <div className="spacerDiv" />
-  <main>
-    <Paper sx={styles.paper}>
-        <Typography variant="h5" align="center" gutterBottom>Checkout</Typography>
-        <Stepper activeStep={activeStep}>
-            {steps.map(step => (
-                <Step key={step}>
-                    <StepLabel>{step}</StepLabel>
-                </Step>
+    <>
+      <div className="spacerDiv" />
+      <main>
+        <Paper sx={styles.paper}>
+          <Typography variant="h5" align="center" gutterBottom>
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
             ))}
-        </Stepper>
-        {activeStep === steps.length ? <Confirmation/> : <Form/>}
-    </Paper>
-  </main>
-  </>
+          </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
+        </Paper>
+      </main>
+    </>
   );
 };
 
