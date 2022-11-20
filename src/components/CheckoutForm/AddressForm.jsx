2572a77fd7fcafcf694e9695db2243ffd5ab24b0
Degisew <jegisew21@gmail.com>
2022-11-20 12:51:32 +0300

    add a logic to fetch shipping countries

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Typography } from "@mui/material";
 import { useForm, FormProvider } from "react-hook-form";
 import FormInputField from "./FormInputField";
 import DropDownField from "./Checkout/DropDownField";
-const AddressForm = () => {
+import commerce from "../../lib/commerce";
+
+const AddressForm = ({ generatedToken }) => {
+  const [shippingCountries, setShippingCountries] = useState([]);
+  const [shippingCounty, setShippingCountry] = useState("");
+  const [shippingSubDivisions, setShippingSubDivisions] = useState([]);
+  const [shippingSubDivision, setShippingSubDivision] = useState("");
+  const [shippingOptions, setShippingOptions] = useState([]);
+  const [shippingOption, setShippingOption] = useState("");
+
+  useEffect(() => {
+    const shippingCountries = async (tokenId) => {
+      // get a list of shipping counries from API and add them to the state.
+      const { countries } = await commerce.services.localeListShippingCountries(
+        tokenId
+      );
+      console.log(countries);
+      setShippingCountries(countries);
+    };
+    shippingCountries(generatedToken.id);
+  }, []);
+
   const methods = useForm();
   return (
     <main className="form-Container">
@@ -15,7 +36,7 @@ const AddressForm = () => {
           action="#"
           onSubmit={methods.handleSubmit((data) => console.log(data))}
         >
-          <Grid container rowSpacing={3} columnSpacing={3}  mt="5px">
+          <Grid container rowSpacing={3} columnSpacing={3} mt="5px">
             <FormInputField name="firstame" />
             <FormInputField name="middleName" />
             <FormInputField name="lastName" />
