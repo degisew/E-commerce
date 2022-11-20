09f302d32cd05cfe50105850c5651ba97184e33e
Degisew <jegisew21@gmail.com>
2022-11-20 12:54:20 +0300

    Add a logic to generate a token

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Paper,
   Stepper,
@@ -8,17 +8,26 @@ import {
 } from "@mui/material";
 import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
+import commerce from '../../../lib/commerce'
 
 import styles from './styles'
 
 const steps = ['shipping address', 'payment details'];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
+  const [generatedToken, setGeneratedToken] = useState(null);
+  useEffect(() => {
+    const token = commerce.checkout.generateToken(cart.id, {type: 'cart'});
+    setGeneratedToken(token);
+  }, [cart]);
     const [activeStep,setActiveStep] = useState(0);
 
-    const Form = () => (
-      activeStep === 0 ? <AddressForm/> : <PaymentForm/>
-    );
+    const Form = () =>
+      activeStep === 0 ? (
+        <AddressForm generatedToken={generatedToken} />
+      ) : (
+        <PaymentForm />
+      );
 
     const Confirmation = () =>  (
       <div>Confirmation</div>
@@ -36,7 +45,7 @@ const Checkout = () => {
                 </Step>
             ))}
         </Stepper>
-        {activeStep === steps.length ? <Confirmation/> : <Form/>}
+        {activeStep === steps.length ? <Confirmation/> : (generatedToken && <Form/>)}
     </Paper>
   </main>
   </>
